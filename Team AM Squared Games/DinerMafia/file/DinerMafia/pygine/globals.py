5b81cac319e2b7b2d3f1564cee44f5642aaa948f
thismarvin <bend_@outlook.com>
2020-01-02 20:39:57 -0600

    fixed compression

--- Team AM Squared Games/DinerMafia/file/DinerMafia/pygine/globals.py
+++
@@ -1,2 +0,0 @@
-debug = False
-money = 0
--- Team AM Squared Games/QuantumCaverns/file/QuantumCaverns/pygine/globals.py
+++
@@ -1,6 +0,0 @@
-# Yeah alright global variables are bad, but give me a break im only using one and it's for debugging purposes!
-debugging = False
-
-def toggle_debugging():
-    global debugging
-    debugging = not debugging
